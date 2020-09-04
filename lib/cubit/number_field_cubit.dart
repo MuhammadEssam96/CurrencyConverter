0afbde01ba0d5f548e59440680d9e92a0d1f5951
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-09-04 19:59:08 +0200

    Setup BlocProvider and NumberFieldCubit

---
+++ lib/cubit/number_field_cubit.dart
@@ -0,0 +1,59 @@
+import 'package:bloc/bloc.dart';
+
+class NumberFieldCubit extends Cubit<NumberFieldState> {
+  NumberFieldCubit() : super(NumberFieldInitialState());
+
+  void addFirstNumber(String number){
+    emit(NumberFieldEditedState(number));
+  }
+
+  void addNumber(String currentNumber, String newNumber){
+    String number = '$currentNumber$newNumber';
+
+    if (number.length <= 10) {
+      emit(NumberFieldEditedState(number));
+    } else {
+      emit(NumberFieldMaxLimitReachedError(currentNumber));
+    }
+  }
+
+  void addDot(String currentNumber) {
+    if(currentNumber.length <= 7 && !currentNumber.contains(".")){
+      currentNumber = "$currentNumber.";
+    }
+    emit(NumberFieldEditedState(currentNumber));
+  }
+
+  void erase(String number){
+    if(number != null && number.length > 1) {
+      String newString = number.substring(0,number.length - 1);
+      emit(NumberFieldEditedState(newString));
+    } else {
+      emit(NumberFieldInitialState());
+    }
+  }
+}
+
+abstract class NumberFieldState {
+  const NumberFieldState();
+
+  String get number => null;
+}
+
+class NumberFieldInitialState extends NumberFieldState {
+  const NumberFieldInitialState();
+}
+
+class NumberFieldEditedState extends NumberFieldState {
+  final String _number;
+  NumberFieldEditedState(this._number);
+
+  String get number => _number;
+}
+
+class NumberFieldMaxLimitReachedError extends NumberFieldState {
+  final String _number;
+  NumberFieldMaxLimitReachedError(this._number);
+
+  String get number => _number;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,34 @@
+import 'package:bloc/bloc.dart';
+import 'package:currency_converter/cubit/observers/number_field_observer.dart';
+import 'package:currency_converter/cubit/number_field_cubit.dart';
 import 'package:currency_converter/ui/screens/home_screen.dart';
 import 'package:currency_converter/ui/screens/splash_screen.dart';
 import 'package:currency_converter/ui/values/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(CurrencyConverterApp());
+void main() {
+  Bloc.observer = NumberFieldObserver();
+  runApp(CurrencyConverterApp());
+}
 
 class CurrencyConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(AppThemes.systemUiOverlayStyle);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Currency Converter",
-      theme: AppThemes.appTheme,
-      initialRoute: "/SplashScreen",
-      routes: {
-        "/SplashScreen" : (context) => SplashScreen(),
-        "/HomeScreen" : (context) => HomeScreen(),
-      },
+    return BlocProvider(
+      create: (context) => NumberFieldCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Currency Converter",
+        theme: AppThemes.appTheme,
+        initialRoute: "/SplashScreen",
+        routes: {
+          "/SplashScreen" : (context) => SplashScreen(),
+          "/HomeScreen" : (context) => HomeScreen(),
+        },
+      ),
     );
   }
 }
