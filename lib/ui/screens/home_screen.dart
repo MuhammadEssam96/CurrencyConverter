c1f81c8cebab8d172fc9029b207af4e3ac1d8356
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-09-21 22:18:26 +0200

    Call the cubits directly when the convert button is pressed

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,7 +1,12 @@
+import 'package:currency_converter/cubits/convert_rate_cubit.dart';
+import 'package:currency_converter/cubits/currency_cubit.dart';
+import 'package:currency_converter/cubits/number_field_cubit.dart';
+import 'package:currency_converter/cubits/result_field_cubit.dart';
 import 'package:currency_converter/ui/values/colors.dart';
 import 'package:currency_converter/ui/widgets/currency_selector_number_field_widget.dart';
 import 'package:currency_converter/ui/widgets/num_pad_row_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -32,8 +37,20 @@ class HomeScreen extends StatelessWidget {
                   CurrencySelectorAndNumberFieldWidget(CurrencyWidgetType.From),
                   IconButton(
                     icon: Icon(Icons.swap_vert),
-                    onPressed: () {
+                    onPressed: () async {
+                      final numberFieldCubit = BlocProvider.of<NumberFieldCubit>(context);
+                      final convertRateCubitFieldCubit = BlocProvider.of<ConvertRateCubit>(context);
+                      final resultFieldCubit = BlocProvider.of<ResultFieldCubit>(context);
 
+                      await convertRateCubitFieldCubit.getConvertRate(
+                        BlocProvider.of<OriginalCurrencyCubit>(context).state.currency.currencyCode,
+                        BlocProvider.of<ConvertedCurrencyCubit>(context).state.currency.currencyCode,
+                      );
+
+                      resultFieldCubit.showResult(
+                        from: numberFieldCubit.state.number,
+                        rate: convertRateCubitFieldCubit.state.convertRate.convertRate.toString()
+                      );
                     },
                   ),
                   CurrencySelectorAndNumberFieldWidget(CurrencyWidgetType.To),
