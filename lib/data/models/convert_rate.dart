0be3dc1ca14b19f374628f6256a181a122bb7b3d
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-09-21 22:07:16 +0200

    Add initial Convert Rate and Currency object models

---
+++ lib/data/models/convert_rate.dart
@@ -0,0 +1,9 @@
+import 'package:currency_converter/data/models/currency.dart';
+
+class ConvertRate {
+  final Currency originalCurrency;
+  final Currency convertedCurrency;
+  final double convertRate;
+
+  const ConvertRate(this.convertRate, {this.originalCurrency, this.convertedCurrency});
+}
---
+++ lib/data/models/currency.dart
@@ -0,0 +1,5 @@
+class Currency {
+  final String _currencyCode;
+  const Currency(this._currencyCode);
+  String get currencyCode => _currencyCode;
+}
